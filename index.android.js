26b64b2fdbe6e5f6d65973c6b18966a1954c7347
Nicole Freed <nicole.freed@gmail.com>
2017-10-16 12:36:37 -0700

    delete unused Text component

--- index.android.js
+++ index.android.js
@@ -1,13 +1,13 @@
 // Import library to help create component
 import React, { Component } from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
 import Header from './src/components/header';
 
 // Create a Component
 export default class App extends Component {
   render() {
     return (
-      <Text><Header></Header></Text>
+      <Header />
     );
   }
 }
