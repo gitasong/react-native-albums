551e4cc2264109e338b86cf3f76c6c6e019d5577
Nicole Freed <nicole.freed@gmail.com>
2017-10-16 12:06:41 -0700

    refactor App as class-based component

--- index.android.js
+++ index.android.js
@@ -1,13 +1,15 @@
 // Import library to help create component
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, AppRegistry } from 'react-native';
 
 // Create a Component
-const App = () => {
-  return (
-    <Text>Some Text</Text>
-  );
-};
+export default class App extends Component {
+  render() {
+    return (
+      <Text>Some Text</Text>
+    );
+  }
+}
 
 // Render component to device
 AppRegistry.registerComponent('albums', () => App);
