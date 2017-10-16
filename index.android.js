deff5106117a03ec4f9a2c6eaea69afee7d613ce
Nicole Freed <nicole.freed@gmail.com>
2017-10-16 13:53:45 -0700

    capitalize Header.js

--- index.android.js
+++ index.android.js
@@ -1,7 +1,7 @@
 // Import library to help create component
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import Header from './src/components/Header';
 
 // Create a Component
 export default class App extends Component {
