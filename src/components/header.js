b3160df2a4bdfe6aa7cadb0dd7440756dd64bc7c
Nicole Freed <nicole.freed@gmail.com>
2017-10-16 12:49:09 -0700

    wrap Text element in View tag

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,16 @@
 // Import library to help create component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Create a Component
 const Header = () => {
   const { textStyle } = styles;
 
-  return <Text style={textStyle}>Albums!</Text>;
+  return (
+    <View>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
+  );
 };
 
 const styles = {
