9031fcb2fddcd34aa6fece49ce36c15f9a37537e
Nicole Freed <nicole.freed@gmail.com>
2017-10-16 12:26:22 -0700

    build out Header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,11 @@
 // Import library to help create component
+import React from 'react';
+import { Text } from 'react-native';
 
 // Create a Component
+const Header = () => {
+  return <Text>Albums!</Text>;
+};
 
 // Make component available to rest of app
+export default Header;
